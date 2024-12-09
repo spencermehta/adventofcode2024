0eb45d8b04e62e87abd77825c24730b75e185c22
Spencer Mehta <spencermehta@gmail.com>
2024-12-09 15:38:40 +0000

    feat: day09 chunks

--- day09.py
+++ day09.py
@@ -14,8 +14,6 @@ def p1():
             for _ in range(c):
                 memory.append('.')
 
-    #print("".join([str(c) for c in memory]))
-
     for i in range(len(memory)-1, -1, -1):
         lowest_free = memory.index('.')
         if lowest_free >= i:
@@ -32,56 +30,55 @@ def p2():
     chars = [int(c) for c in line]
     file_number = 0
     memory = []
+    memory_len = 0
+
     files = []
+    empties = []
+
     for i, c in enumerate(chars):
         if i % 2 == 0:
-            files.append((file_number, c, len(memory)))
+            files.append((file_number, c, memory_len))
             for _ in range(c):
                 memory.append(file_number)
+            memory_len += c
             file_number+=1
         else:
+            empties.append((c, memory_len))
             for _ in range(c):
                 memory.append('.')
+            memory_len += c
 
-    #print("".join([str(c) for c in memory]))
-    for j in range(len(files)-1, -1, -1):
-        (file_num, file_length, file_start) = files[j]
-        #print("\n\n", file_num, file_length, "\n\n")
+    L = ['.' for _ in memory]
+    for i in range(len(files)-1, -1, -1):
+        file_num, file_len, file_start = files[i]
 
-        lowest_free, lowest_free_length = get_lowest_free(memory)
+        empty_index = 0
+        empty_len, empty_start = empties[empty_index]
+        while empty_len < file_len:
+            empty_index += 1
+            if len(empties) == empty_index:
+                empty_len, empty_start = 0, 1
+                break
+            empty_len, empty_start = empties[empty_index]
 
-        #print(lowest_free, lowest_free_length)
-        #print("".join([str(c) for c in memory]))
-        while lowest_free_length < file_length and lowest_free_length != 0:
-            #print("looping")
-            olf = lowest_free
-            olfl = lowest_free_length
-            lowest_free, lowest_free_length = get_lowest_free(memory[lowest_free + lowest_free_length+1:])
-            lowest_free += olf + olfl + 1
-            #print(lowest_free, lowest_free_length)
-            #print("".join([str(c) for c in memory]))
+        if file_start < empty_start or empty_len < file_len:
+            for i in range(file_start, file_start + file_len):
+                L[i] = file_num
+            continue
 
-        if lowest_free_length >= file_length and lowest_free < file_start:
-            #print("writing")
-            for i in range(file_length):
-                memory[lowest_free + i] = memory[file_start + i]
-                memory[file_start + i] = '.'
+        new_file_start = empty_start
 
+        for i in range(new_file_start, new_file_start + file_len):
+            L[i] = file_num
 
-    #print("".join([str(c) for c in memory]))
-    print(checksum(memory))
+        new_empty_len = empty_len - file_len
+        if new_empty_len == 0:
+            del empties[empty_index]
+        else:
+            empties[empty_index] = (new_empty_len, empty_start + file_len)
 
-def get_lowest_free(memory):
-    if '.' not in memory:
-        return 0, 0
-    lowest_free = memory.index('.')
-    lowest_free_length = 1
-    while memory[lowest_free + lowest_free_length] == '.':
-        lowest_free_length+=1
-        if len(memory) <= lowest_free + lowest_free_length:
-            break
-    return lowest_free, lowest_free_length
-
+    # print("".join([str(c) for c in L]))
+    print(checksum(L))
 
 def checksum(arr):
     tot = 0
