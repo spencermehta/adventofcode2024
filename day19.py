b6e2dc0b5459016e4d53eb2f8bdb9ea164ebd8bb
Spencer Mehta <spencermehta@gmail.com>
2024-12-20 20:09:55 +0000

    feat: day19part2

--- day19.py
+++ day19.py
@@ -7,47 +7,82 @@ with open(file_name) as f:
 towels, patterns = f.split("\n\n")
 towels = towels.replace(' ', '').split(',')
 patterns = patterns.split("\n")
-print(towels)
+
+C1 = {}
+C = {}
+CC = {}
 
 def p1():
     tot = 0
     for pattern in patterns:
-        print(f"{pattern=}")
         if can_make_pattern(pattern):
             tot+=1
     print(tot)
 
-C = {}
-def can_make_pattern(pattern):
+def p2():
+    tot = 0
+    for pattern in patterns:
+        pats = can_make_pattern(pattern)
+        tot+=pats
+    print(tot)
+
+def can_make_pattern_p1(pattern):
     i=len(pattern)
 
     while i>=0:
-        if (pattern, i) in C:
+        if (pattern, i) in C1:
             return C[(pattern, i)]
         pi = pattern[:i]
-        #print(f"{i=} {pi=} ")
         if pi not in towels:
             i=i-1
             continue
 
         remaining = pattern[i:]
         if remaining == "":
-            C[(pattern, i)] = True
+            C1[(pattern, i)] = True
             return True
-        print(f"continuing with {remaining=}")
-        cmp = can_make_pattern(remaining)
+        cmp = can_make_pattern_p1(remaining)
         if not cmp:
-            C[(pattern, i)] = False
+            C1[(pattern, i)] = False
             i=i-1
-            print(f"backtracking at {i=}")
             continue
 
         return True
 
     return False
 
+def can_make_pattern(pattern):
+    i=len(pattern)
+
+    count = 0
+    while i>=0:
+        if (pattern, i) in C:
+            return C[(pattern,i)]
+
+        pi = pattern[:i]
+        if pi not in towels:
+            i=i-1
+            continue
+
+        remaining = pattern[i:]
+        if remaining == "":
+            count+=1
+
+        if (remaining) in CC:
+            cmp = CC[remaining]
+        else:
+            cmp = can_make_pattern(remaining)
+            CC[remaining] = cmp
+        count+=cmp
+
+        i=i-1
+
+    C[(pattern,i)] = count
+    return count
+
 
 
 
 p1()
+p2()
 
