71c4fe22ecf2606a90acb703c6e1defbf0c08361
Spencer Mehta <s@spencermehta.com>
2024-12-22 16:46:41 +0000

    feat: day22part2

--- day22.py
+++ day22.py
@@ -20,58 +20,65 @@ def p1():
 def p2():
     all_seqs = []
     all_prices = []
+    sequences_to_prices = []
     for num in lines:
         sequences, prices = simulate_buyer(num)
         all_seqs.append(sequences)
         all_prices.append(prices)
+        seq_to_price = get_sequence_to_price(sequences, prices)
+        sequences_to_prices.append(seq_to_price)
 
     best = 0
-    for i in range(len(lines[:1])):
+    buy_sequences = set()
+    for i in range(len(lines)):
+        print(f"{i=}")
         sequences = all_seqs[i]
-        for low in range(2000-3):
+        for low in range(2000-4):
             high = low+4
-            buy_seq = sequences[low:high]
+            buy_seq = tuple(sequences[low:high])
+            buy_sequences.add(buy_seq)
+    print(len(buy_sequences))
 
-            tot = 0
-            for num in lines:
-                price = simulate_buyer_buying(num, buy_seq)
-                if price is not None:
-                    tot+=price
-            if tot > best:
-                best = tot
-    print(best)
+    for i, buy_seq in enumerate(buy_sequences):
+        if i % 1000 == 0:
+            print(f"{i=}")
+        tots = []
+        for j in range(len(lines)):
+            if buy_seq in sequences_to_prices[j]:
+                price = sequences_to_prices[j][buy_seq]
+                tots.append(price)
+                if buy_seq == tuple([-2,1,-1,3]):
+                    print(f"{j=} {tots=}")
 
 
-def simulate_buyer_buying(num, seq):
-    sequences = []
-    prices = []
-    n = num
-    price = int(str(n)[len(str(n))-1:len(str(n))])
-    for _ in range(2000):
-        prev = price
-        n = simulate_day(n)
-        price = int(str(n)[len(str(n))-1:len(str(n))])
-        sequences.append(price-prev)
-        prices.append(price)
+        if sum(tots) > best:
+            best = sum(tots)
+            print(f"best {best=} found at {i=} from {tots=} and {buy_seq=}")
+    print(f"{best=}")
 
-        if sequences[len(sequences)-4:len(sequences)] == seq:
-            return price
-    return None
+def get_sequence_to_price(sequences, prices):
+    sequence_map = {}
+    for low in range(len(sequences)-4):
+        high = low + 4
+        buy_seq = tuple(sequences[low:high])
+        if buy_seq not in sequence_map:
+            sequence_map[buy_seq] = prices[high-1]
+        
+    return sequence_map
+
 
 def simulate_buyer(num):
     sequences = []
     prices = []
     n = num
     price = int(str(n)[len(str(n))-1:len(str(n))])
-    for _ in range(2000):
+    for _ in range(2001):
         prev = price
         n = simulate_day(n)
         price = int(str(n)[len(str(n))-1:len(str(n))])
         sequences.append(price-prev)
         prices.append(price)
     return sequences, prices
-
-
 
 def simulate_day(num):
     big = num * 64
