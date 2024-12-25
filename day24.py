e1f97b1532aa467209c32d8e99dc36f6f2a75e93
Spencer Mehta <s@spencermehta.com>
2024-12-25 14:17:51 +0000

    feat: day24 p2

--- day24.py
+++ day24.py
@@ -62,15 +62,36 @@ def p1():
     print(anss)
 
 def p2():
-    print(pp("z00"))
+    pairs = [
+        ("thm", "z08"),
+        ("wrm", "wss"),
+        ("hwq", "z22"),
+        ["gbs", "z29"]
 
-    for i in range(44):
-        s = ["0" for _ in range(44)]
-        s[i] = "1"
-        bin_num = int(s, 2)
+    ]
 
-def ppp(wire):
-    print(pp(wire))
+    for l,r in pairs:
+        swap(l, r)
+
+    i = 0
+    while True:
+        if not verify(i):
+            break
+        i+=1
+    print(i)
+
+    all_pairs = []
+    for l,r in pairs:
+        all_pairs.append(l)
+        all_pairs.append(r)
+    print(",".join(sorted(all_pairs)))
+def swap(left, right):
+    temp = ops[left]
+    ops[left] = ops[right]
+    ops[right] = temp
+
+def ppp(num):
+    print(pp(make_wire("z", num)))
 
 def pp(wire, d=0):
     if wire[0] in "xy":
@@ -78,6 +99,58 @@ def pp(wire, d=0):
     
     op, left, right = ops[wire]
     return d * "  " + op + "(" + wire + ")\n" + pp(left, d+1) + "\n" + pp(right, d+1)
+
+# watched hyperneutrino's video for tips
+def verify_z(wire, num):
+    print("vz", wire, num)
+    op, left, right = ops[wire]
+
+    if op != "XOR":
+        return False
+    if num == 0:
+        return (sorted([left, right]) == ["x00", "y00"])
+
+    return verify_intermediate_xor(left, num) and verify_carry_bit(right, num) or verify_intermediate_xor(right, num) and verify_carry_bit(left, num)
+
+def verify_intermediate_xor(wire, num):
+    print("vx", wire, num)
+    op, left, right = ops[wire]
+    if op != "XOR": return False 
+    return sorted([left, right]) == [make_wire("x", num), make_wire("y", num)]
+
+def verify_carry_bit(wire, num):
+    print("vc", wire, num)
+    op, left, right = ops[wire]
+    if num == 1:
+        if op != "AND":
+            return False
+        return sorted([left, right]) == ["x00", "y00"]
+    if op != "OR":
+        return False 
+    return verify_direct_carry(left, num - 1) and verify_recarry(right, num - 1) or verify_direct_carry(right, num - 1) and verify_recarry(left, num - 1)
+
+def verify_direct_carry(wire, num):
+    print("vd", wire, num)
+    op, left, right = ops[wire]
+    if op != "AND":
+        return False
+    return sorted([left, right]) == [make_wire("x", num), make_wire("y", num)]
+
+def verify_recarry(wire, num):
+    print("vr", wire, num)
+    op, left, right = ops[wire]
+    if op != "AND":
+        return False
+    return verify_intermediate_xor(left, num) and verify_carry_bit(right, num) or verify_intermediate_xor(right, num) and verify_carry_bit(left, num)
+
+def verify(num):
+    return verify_z(make_wire("z", num), num)
+
+def make_wire(char, num):
+    return char + str(num).rjust(2, "0")
+
+
+
 
 #p1()
 p2()
